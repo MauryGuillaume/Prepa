e65f1a2c89884df56e49104186a35892d82a965b
Guillaume Maury <guillaumemaury33700@gmail.com>
2025-07-20 12:07:42 +0200

    update

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'handball-cache-v4'; // 🔁 Mets à jour ce nom à chaque nouvelle version
+const CACHE_NAME = 'handball-cache-v5'; // 🔁 Mets à jour ce nom à chaque nouvelle version
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './sw.js',
-  './icon-192.png',
-  './icon-512.png'
+  //'./icon-192.png',
+  //'./icon-512.png'
 ];
 
 // INSTALLATION — mise en cache
